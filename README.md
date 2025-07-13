2aa9c186762520caa9172fbc31c4c085d7e2e945
Shree Harsha <shsmysore@gmail.com>
2025-07-13 20:11:52 +0530

    Controlled Vs Uncontrolled components.

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Positioning the child element using transform, if we need to acconunt for its ow
 
 * Progress updates
 - Use React state for the meme properties. Refactor.
+
+- Controlled Vs Uncontrolled components.
+Example is a text view. If a React state doesn't output the view (contents) its an "uncontrolled component".
+Once the value on the text shown in coming from a React state its called "controlled component".
+
+When React doesn't control the text shown to user, the inherent memory on the html element takes care of showing the typed in value. This may lead to confusions. What the React application is thinking the current value shown to the user doesn't match with reality.
--- src/MemeApp.jsx
+++ src/MemeApp.jsx
@@ -4,10 +4,26 @@ import { useState } from 'react';
 
 export default function MemeApp() {
     const [meme, setMeme] = useState({
-        topText: 'Top Text',
-        bottomText: 'Bottom Text',
+        topText: 'One does not simply',
+        bottomText: 'Walk into Mordor',
         imageUrl: 'https://i.imgflip.com/1bij.jpg',
     });
+
+    function handleTextOnChange(event) {
+        const newValue = event.target.value;
+        const valueOn = event.target.name;
+        if (valueOn === 'topText') {
+            setMeme(prevMeme => ({
+                ...prevMeme,
+                topText: newValue
+            }));
+        } else if (valueOn === 'bottomText') {
+            setMeme(prevMeme => ({
+                ...prevMeme,
+                bottomText: newValue
+            }));
+        }
+    }
 
     return (
         <div className="meme-app">
@@ -17,11 +33,15 @@ export default function MemeApp() {
                     <div className="meme-form-row">
                         <label>
                             Top text
-                            <input type="text" name="topText" />
+                            <input type="text" name="topText" 
+                                onChange={handleTextOnChange}
+                                value={meme.topText}/>
                         </label>
                         <label>
                             Bottom text
-                            <input type="text" name="bottomText" />
+                            <input type="text" name="bottomText" 
+                                onChange={handleTextOnChange}
+                                value={meme.bottomText}/>
                         </label>
                     </div>
                     <button type="submit">Generate Meme</button>
