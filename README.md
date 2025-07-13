e3bd83cdee9770087817f8e6a779271bbdccc5e2
Shree Harsha <shsmysore@gmail.com>
2025-07-13 19:33:06 +0530

    Refactor to use the React state on meme properties.

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ The css is used while overlaying the text on top of meme image.
 Related css: properties like top, left, right, bottom for child element. 
 Positioning the child element using transform, if we need to acconunt for its own width/height.
 
-* 
+* Progress updates
+- Use React state for the meme properties. Refactor.
--- src/MemeApp.jsx
+++ src/MemeApp.jsx
@@ -1,7 +1,14 @@
 import './MemeApp.css'
 import Header from './components/Header.jsx'
+import { useState } from 'react';   
 
 export default function MemeApp() {
+    const [meme, setMeme] = useState({
+        topText: 'Top Text',
+        bottomText: 'Bottom Text',
+        imageUrl: 'https://i.imgflip.com/1bij.jpg',
+    });
+
     return (
         <div className="meme-app">
             <Header />
@@ -22,12 +29,12 @@ export default function MemeApp() {
                 <div className="meme-image-container">
                     <section className="meme-display">
                     <img
-                        src="https://i.imgflip.com/1bij.jpg"
+                        src={meme.imageUrl}
                         alt="Meme Example"
                         className="meme-image"
                     />
-                    <span className="top-text">Top Text</span>
-                    <span className="bottom-text">Bottom Text</span>
+                    <span className="top-text">{meme.topText}</span>
+                    <span className="bottom-text">{meme.bottomText}</span>
                 </section>
                 </div>
             </main>
